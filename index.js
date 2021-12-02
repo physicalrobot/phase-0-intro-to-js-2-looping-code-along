abe10d41e3befef398c70328d1bbbfdbc34aa1f6
Vikalp Malhotra <kimboho@gmail.com>
2021-12-02 16:28:42 -0500

    still working

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+const p = ['Guadalupe', 'Ollie', 'Aki'];
+function writeCards(name, event) {
+
+
+
+    for (let i = 0; i < name.length; i++) {
+        let namelist = [];
+
+
+        namelist[i] = console.log(`Thank you, ${name[i]} for the wonderful surprise ${event} gift!`);
+    }
+    return namelist;
+
+}
+
+writeCards(p, 'birthday');
